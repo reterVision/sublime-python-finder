56388ed41e154d326acbeb45928ce5b1a301b537
reterVision <reterclose@gmail.com>
2013-07-13 19:30:44 +0800

    Optimize the keyword search, first analyze current file to precisely locate the imported source files then search for defintion. Hope this can give better result and performance.

--- python_finder.py
+++ python_finder.py
@@ -1,12 +1,94 @@
 import os
 import re
-import sys
 import threading
 import sublime
 import sublime_plugin
 
 
 DEF_NOT_FOUND = 'Not Found'
+
+
+def get_imported_source(file_name):
+    source_name = {}
+    file_content = ''
+    source = ''
+    word = ''
+    from_tag = False
+    import_tag = False
+    import_brackets_tag = False
+    import_comma_tag = False
+    next_line_tag = False
+
+    try:
+        with open(file_name, 'rb') as f:
+            for line in f:
+                file_content += line
+    except IOError:
+        return source_name
+
+    for w in file_content:
+        if not w:
+            continue
+        if w == '\n':
+            next_line_tag = False
+            import_comma_tag = False
+        elif w != ' ' and w != '\t':
+            word += w
+            continue
+
+        if not word:
+            continue
+        elif word == 'from':
+            from_tag = True
+            import_brackets_tag = False
+            import_comma_tag = False
+            word = ''
+        elif word == 'import':
+            import_tag = True
+            import_brackets_tag = False
+            import_comma_tag = False
+            word = ''
+        else:
+            if from_tag:
+                upper_dir = 0
+                while upper_dir < len(word) - 1 and word[upper_dir] == '.':
+                    upper_dir += 1
+                word = word[upper_dir:]
+                if upper_dir != 0:
+                    folder = '/'.join(file_name.split('/')[:-upper_dir])
+                    word = folder + '/' + word
+                source = word
+                if source not in source_name:
+                    source_name[source] = []
+                from_tag = False
+            elif import_tag:
+                replace_str = []
+                if '(' in word:
+                    replace_str.append('(')
+                    import_brackets_tag = True
+                if ')' in word:
+                    replace_str.append(')')
+                    import_brackets_tag = False
+                if ',' in word:
+                    replace_str.append(',')
+                    import_comma_tag = True
+                if '\\' in word:
+                    replace_str.append('\\')
+                    next_line_tag = True
+                for s in replace_str:
+                    word = word.replace(s, '')
+
+                if word:
+                    try:
+                        source_name[source].append(word)
+                    except KeyError:
+                        source_name[word] = [word]
+
+                if not (import_brackets_tag or
+                        import_comma_tag or next_line_tag):
+                    import_tag = False
+            word = ''
+    return source_name
 
 
 class PythonFinderCommand(sublime_plugin.TextCommand):
@@ -50,38 +132,26 @@ class PythonFinderCommand(sublime_plugin.TextCommand):
 
         for key in keywords:
             imported = key.split('.')[0]
-            result = self.view.find_all(
-                r'\bfrom\s+(\S+\s)import.+{0}'.format(imported)
-            )
-            if not result:
-                result = self.view.find_all(
-                    r'\bimport.+({0})'.format(imported)
-                )
-                for r in result:
-                    s = self.view.substr(r)
-                    r = s.replace('import', '').strip().replace('.', '/')
-                    imported_sources.append(r)
-            else:
-                for r in result:
-                    s = self.view.substr(r)
-                    r = s.replace('import', '').replace('from', '').strip()
-                    source_path = r.split(' ')[0].strip().replace('.', '/')
-                    imported_sources.append(source_path)
-        imported_sources = list(set(imported_sources))
+            source_name = get_imported_source(self.view.file_name())
+            for k in source_name:
+                if imported in source_name[k]:
+                    imported_sources.append(k.replace('.', '/'))
 
-        # Get search path, starts from system path then user path.
-        system_path = [p for p in sys.path
-                       if os.path.isdir(p) and
-                       p[-4:] != '.egg' and p[-9:] != '.egg-info']
+        # Get search path
         try:
             opened_folder = self.view.window().folders()[0] + '/'
         except IndexError:
             opened_folder = '.'
-        search_path = system_path + [opened_folder]
+        search_path = [opened_folder]
+
+        # Trim the imported sources, mainly for relative import
+        trimmed_sources = []
+        for s in imported_sources:
+            trimmed_sources.append(s.replace(opened_folder, ''))
 
         # Start a thread to do the search, avoid affecting normal use.
         threads = []
-        thread = KeywordSearch(keywords, imported_sources, search_path)
+        thread = KeywordSearch(keywords, trimmed_sources, search_path)
         threads.append(thread)
         thread.start()
 
@@ -110,8 +180,12 @@ class PythonFinderCommand(sublime_plugin.TextCommand):
         self.show_result()
 
     def show_result(self):
+        """
+        Display the search result in a popup panel.
+        """
         self.view.sel().clear()
         current_window = self.view.window()
+        self.result_list = list(set(self.result_list))
         self.result_list = self.result_list or [DEF_NOT_FOUND]
         current_window.show_quick_panel(self.result_list, self.open_selected)
 
@@ -145,39 +219,40 @@ class KeywordSearch(threading.Thread):
             self.search(self.imports, keyword)
 
     def get_func_pattern(self, keyword):
-        return re.compile(r'def\s+{0}\s*\(.*\)\s*\:'.format(keyword))
+        return re.compile(r'def\s+{0}\s*\(.*'.format(keyword))
 
     def get_class_pattern(self, keyword):
         return re.compile(r'class\s+{0}\s*\(.*\)\s*\:'.format(keyword))
+
+    def detect_keyword_file(self, file_name):
+        try:
+            with open(file_name, 'rb'):
+                result = '{0};{1};'.format(file_name, 1)
+                self.result_list.append(result)
+                return True
+        except IOError:
+            return False
 
     def search(self, imports, keyword):
         for source in imports:
             for path in self.search_path:
                 file_name = os.path.join(path, source)
+
                 if os.path.isdir(file_name):
-                    file_name += '/__init__.py'
-                    from_import_pattern = re.compile(
-                        r'\bfrom\s+(\S+\s)import\s+{0}'.format(keyword),
-                        flags=re.IGNORECASE)
-                    import_all_pattern = re.compile(
-                        r'\bfrom\s+(\S+{0}\s)import\s+\*'.format(keyword),
-                        flags=re.IGNORECASE)
+                    init_file_name = file_name + '/__init__.py'
                     imported_sources = []
-                    try:
-                        with open(file_name, 'rb') as f:
-                            for i, l in enumerate(f):
-                                result = from_import_pattern.findall(l)
-                                for r in result:
-                                    imported_sources.append(
-                                        r.strip().replace('.', '/'))
-                                result = import_all_pattern.findall(l)
-                                for r in result:
-                                    imported_sources.append(
-                                        r.strip().replace('.', '/'))
-                    except IOError:
+                    source_name = get_imported_source(init_file_name)
+                    for k in source_name:
+                        if keyword in source_name[k]:
+                            imported_sources.append(k.replace('.', '/'))
+                    if not imported_sources:
+                        imported_sources.append('__init__')
+                    self.search_path.append(file_name)
+                    self.search(imported_sources, keyword)
+
+                    keyword_file_name = file_name + '/' + keyword + '.py'
+                    if self.detect_keyword_file(keyword_file_name):
                         continue
-                    if imported_sources:
-                        self.search(imported_sources, keyword)
                 else:
                     file_name += '.py'
                 try:
